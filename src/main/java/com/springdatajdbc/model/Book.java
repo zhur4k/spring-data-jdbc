e669dfb0c475fa46a69f76fd964ba228a94b7d71
zhur4k <kostya.zhurovich12052@gmail.com>
2024-12-25 07:43:18 +0300

    feat:create book entity.

---
+++ src/main/java/com/springdatajdbc/model/Book.java
@@ -0,0 +1,10 @@
+package com.springdatajdbc.model;
+
+import java.time.LocalDate;
+
+public class Book {
+    private Long id;
+    private String title;
+    private String author;
+    private LocalDate publicationYear;
+}
