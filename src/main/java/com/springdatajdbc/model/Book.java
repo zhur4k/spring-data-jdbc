d76ce67a83ce22f26a253f81dfbfcd3310c822c8
zhur4k <kostya.zhurovich12052@gmail.com>
2024-12-25 07:56:46 +0300

    feat:create getters, setters and add id annotation.

--- src/main/java/com/springdatajdbc/model/Book.java
+++ src/main/java/com/springdatajdbc/model/Book.java
@@ -1,10 +1,48 @@
 package com.springdatajdbc.model;
 
+import org.springframework.data.annotation.Id;
+
 import java.time.LocalDate;
 
 public class Book {
+    @Id
     private Long id;
+
     private String title;
+
     private String author;
+
     private LocalDate publicationYear;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getAuthor() {
+        return author;
+    }
+
+    public void setAuthor(String author) {
+        this.author = author;
+    }
+
+    public LocalDate getPublicationYear() {
+        return publicationYear;
+    }
+
+    public void setPublicationYear(LocalDate publicationYear) {
+        this.publicationYear = publicationYear;
+    }
 }
